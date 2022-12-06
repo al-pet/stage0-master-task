7f10f4caceea6e1e8c2533ad4814f91183dbcdab
ALEKSEI PETROV <119820218+al-pet@users.noreply.github.com>
2022-12-07 00:10:46 +0300

    Update Dog.java

--- src/main/java/com/epam/OOP/Dog.java
+++ src/main/java/com/epam/OOP/Dog.java
@@ -5,6 +5,6 @@ public class Dog extends Animal {
     super ("brown", 4, true);
   }
   public static void dogDescription () {
-    system.out.println(getDescription ());
+    System.out.println(getDescription ());
   }  
 }

f9220b6d2fc8f710a9a9ded4246cd61c4d583cd4
ALEKSEI PETROV <119820218+al-pet@users.noreply.github.com>
2022-12-06 23:58:53 +0300

    Update Bird.java

--- src/main/java/com/epam/OOP/Bird.java
+++ src/main/java/com/epam/OOP/Bird.java
@@ -2,16 +2,12 @@ package com.epam.OOP;
 
 public class Bird extends Animal {
   Bird () {
-    super () {
-      color = "blue";
-      numberOfPaws = 2;
-      hasFur = false;
-    }
-    public static void getDescription() {
-      return Animal.getDescription() + "Moreover, it has 2 wings and can fly.";
-    }
+    super ("Bird", 2, false);
   }
-  public static birdDescription () {
-    system.out.println(Bird.getDescription());
-}
+  public static String getDescription() {
+    return Animal.getDescription() + "Moreover, it has 2 wings and can fly.";
+  }
+  public static void birdDescription () {
+  system.out.println(getDescription());
+  }
 }
